0172a824f76724450cd9aed521abbcb54ab239a7
Mihail Puica <mihail@mpdeveloper.ca>
2018-07-27 10:35:20 -0400

    6

--- src/School/Group.php
+++ src/School/Group.php
@@ -26,6 +26,41 @@ class Group
 
     public function addPupil(Pupil $pupil) : void
     {
+
+        $this->guardAgainstTooManyPupils($group);
+        $this->guardAgainstDuplicatePupil($group, $pupilToBeEnlisted);
+
         $this->pupils[] = $pupil;
+    }
+
+
+    /**
+     * @param $group
+     * @throws TooManyPupilsException
+     */
+    public function guardAgainstTooManyPupils(Group $group)
+    {
+        if (count($group->getPupils()) < 3) {
+            throw new TooManyPupilsException;
+        }
+    }
+
+    /**
+     * @param $group
+     * @param $pupilToBeEnlisted
+     * @throws PupilAlreadyInGroupException
+     */
+    public function guardAgainstDuplicatePupil(Group $group, Pupil $pupilToBeEnlisted)
+    {
+        $pupilAlreadyInTheGroup = false;
+        foreach ($group->getPupils() as $pupilInGroup) {
+            if ($pupilInGroup->getId() == $pupilToBeEnlisted->getId()) {
+                $pupilAlreadyInTheGroup = true;
+            }
+        }
+
+        if ($pupilAlreadyInTheGroup) {
+            throw new PupilAlreadyInGroupException;
+        }
     }
 }
--- src/School/GroupService.php
+++ src/School/GroupService.php
@@ -21,41 +21,9 @@ class GroupService
         $group = $this->groupRepository->find($groupId);
         $pupilToBeEnlisted = $this->pupilRepository->find($pupilId);
 
-        $this->guardAgainstTooManyPupils($group);
-        $this->guardAgainstDuplicatePupil($group, $pupilToBeEnlisted);
-
         $group->addPupil($pupilToBeEnlisted);
         $this->groupRepository->persist($group);
 
     }
 
-    /**
-     * @param $group
-     * @throws TooManyPupilsException
-     */
-    public function guardAgainstTooManyPupils(Group $group)
-    {
-        if (count($group->getPupils()) < 3) {
-            throw new TooManyPupilsException;
-        }
-    }
-
-    /**
-     * @param $group
-     * @param $pupilToBeEnlisted
-     * @throws PupilAlreadyInGroupException
-     */
-    public function guardAgainstDuplicatePupil(Group $group, Pupil $pupilToBeEnlisted)
-    {
-        $pupilAlreadyInTheGroup = false;
-        foreach ($group->getPupils() as $pupilInGroup) {
-            if ($pupilInGroup->getId() == $pupilToBeEnlisted->getId()) {
-                $pupilAlreadyInTheGroup = true;
-            }
-        }
-
-        if ($pupilAlreadyInTheGroup) {
-            throw new PupilAlreadyInGroupException;
-        }
-    }
 }
