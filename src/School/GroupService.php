bf13478f07c7e7d64d57fa9b3a4ce1930da51db4
Mihail Puica <mihail@mpdeveloper.ca>
2018-07-27 10:17:54 -0400

    5

--- src/School/GroupService.php
+++ src/School/GroupService.php
@@ -19,13 +19,34 @@ class GroupService
     public function enlistPupilInGroup($groupId, $pupilId)
     {
         $group = $this->groupRepository->find($groupId);
-
         $pupilToBeEnlisted = $this->pupilRepository->find($pupilId);
 
+        $this->guardAgainstTooManyPupils($group);
+        $this->guardAgainstDuplicatePupil($group, $pupilToBeEnlisted);
+
+        $group->addPupil($pupilToBeEnlisted);
+        $this->groupRepository->persist($group);
+
+    }
+
+    /**
+     * @param $group
+     * @throws TooManyPupilsException
+     */
+    public function guardAgainstTooManyPupils(Group $group)
+    {
         if (count($group->getPupils()) < 3) {
             throw new TooManyPupilsException;
         }
+    }
 
+    /**
+     * @param $group
+     * @param $pupilToBeEnlisted
+     * @throws PupilAlreadyInGroupException
+     */
+    public function guardAgainstDuplicatePupil(Group $group, Pupil $pupilToBeEnlisted)
+    {
         $pupilAlreadyInTheGroup = false;
         foreach ($group->getPupils() as $pupilInGroup) {
             if ($pupilInGroup->getId() == $pupilToBeEnlisted->getId()) {
@@ -36,9 +57,5 @@ class GroupService
         if ($pupilAlreadyInTheGroup) {
             throw new PupilAlreadyInGroupException;
         }
-
-        $group->addPupil($pupilToBeEnlisted);
-        $this->groupRepository->persist($group);
-
     }
 }
